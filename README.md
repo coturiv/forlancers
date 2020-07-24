216bdcd1fa1c0aedca0e8020c0df08286b9b8f42
coturiv <coturiv@gmail.com>
2020-07-24 19:11:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# forlancers
+
+[![build](https://github.com/coturiv/forlancers/workflows/build/badge.svg)](https://github.com/coturiv/forlancers/actions)
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PrimaryButton>I am a button.</PrimaryButton>
+      </header>
+    </div>
+  );
+}
+
+export default App;
